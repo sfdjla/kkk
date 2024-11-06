abf85e0cb4e11ee03b04c96ec61495b798d7a2b8
sfdjla <23hyeonmin_yu@dsuyoung.ms.kr>
2024-11-06 17:01:31 +0900

    first commit

---
+++ home.py
@@ -0,0 +1,31 @@
+import streamlit as st
+st.header("브롤스타즈 잘하려면 이 사이트 봐")
+st.subheader("세팅법")
+st.image("https://th.bing.com/th/id/OIP.eGAFy2Xc_wcasR1VEmD1BAHaFj?w=237&h=180&c=7&r=0&o=5&pid=1.7")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ test.py
@@ -0,0 +1,6 @@
+import random
+notsolo = random.randint(1,100) < 1
+if notsolo :
+    print("couple")
+else :
+    print("sibal solo")
